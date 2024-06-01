5308817cf5c16d902e07fa354d99d8a36ae9a345
Most Khal <mostafa6khaled@gmail.com>
2024-06-01 14:30:04 +0300

    updating main.cpp copied from tasks version 2 gfx-projects

--- main.cpp
+++ main.cpp
@@ -1,180 +1,188 @@
 #include <glad/gl.h>
-#include <iostream>
 
-#include "graphics.hpp"
 #include "system.hpp"
+#include "graphics.hpp"
 
+void handle_input(GLFWwindow* window, gfx::vector3f& eye, float dt, std::function<float(float)> ease);
 
-void handleInput(GLFWwindow* window, gfx::vbuffer<gfx::vertex3d>& vertex_buffer, float dt, std::function<float(float)> func);
+bool pressed = false;
 
-void lerp(gfx::vbuffer<gfx::vertex3d>& vbuff, const gfx::vector3f& centre, const gfx::vector3f& end, float size, float t)
+void key(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-  
-  vbuff[0].x = gfx::lerp(centre.x + size, end.x + size, t);
-  vbuff[0].y = gfx::lerp(centre.y - size, end.y - size, t);
+  if(key == GLFW_KEY_J && action == GLFW_PRESS)
+    ::pressed = true;
+  else if(key == GLFW_KEY_J && action == GLFW_RELEASE)
+    ::pressed = false;
 
-  vbuff[1].x = gfx::lerp(centre.x - size, end.x - size, t);
-  vbuff[1].y = gfx::lerp(centre.y - size, end.y - size, t);
-
-  vbuff[2].x = gfx::lerp(centre.x + size, end.x + size, t);
-  vbuff[2].y = gfx::lerp(centre.y + size, end.y + size, t);
-
-  vbuff[3].x = gfx::lerp(centre.x - size, end.x - size, t);
-  vbuff[3].y = gfx::lerp(centre.y + size, end.y + size, t);
-
-  vbuff.modify();
+  gfx::callback::key(window, key, scancode, action, mods);
 }
 
-int main()
+int main(int argc, const char* argv[])
 {
-  constexpr int width  = 700;
-  constexpr int height = 700;
+  const int width  = 700;
+  const int height = 700;
 
   gfx::context context;
-  context.setVersion({ 2 , 0 });
+  context.setVersion({2, 0});
   context.setWindowData(width, height, "gfx-test");
 
-  //a window is created in here. you can get a pointer to that window
   bool result = context.init();
-  GLFW_ASSERT(result, "failed to init gfx context");
-
-  gfx::vector3f centre(-0.9f , -0.9f, -1.f);
-  float size = 0.2f;
-
-  auto color = gfx::hex(0xcc241dff);
-
-  gfx::vertex3d_buf vertex_buffer;
-  vertex_buffer.bind();
-  vertex_buffer.append(gfx::vertex3d(centre.x + size, centre.y - size, centre.z, color));
-  vertex_buffer.append(gfx::vertex3d(centre.x - size, centre.y - size, centre.z, color));
-  vertex_buffer.append(gfx::vertex3d(centre.x + size, centre.y + size, centre.z, color));
-  vertex_buffer.append(gfx::vertex3d(centre.x - size, centre.y + size, centre.z, color));
-  
-  vertex_buffer.load_data(GL_STATIC_DRAW);
-  
-  gfx::program prg;
-  prg.create("/home/sasa/shaders/shader3d.vert", "/home/sasa/shaders/shader3d.frag");
-  prg.link();
-
-  int mvp_location  = prg.getUniformLocation("MVP");
-  gfx::attributes::vpos_location() = prg.getAttribLocation("vPos");
-  gfx::attributes::vcol_location() = prg.getAttribLocation("vCol");
+  GLFW_ASSERT(result, "failed to create gfx context");
 
   GLFWwindow* window;
   context.getWindow(window);
 
-  auto tp = gfx::datetime(2006, 10, 5, 27, 20);
-  std::cout << gfx::strtime(tp);
-  
-  glfwSetKeyCallback(window, gfx::callback::key);
-  gfx::clock c, cf;
-  int   frq;
-  float dt;
+  glfwSetKeyCallback(window, key);
 
-  gfx::view vw(width, height);
+  gfx::program program;
+  program.create(
+      "/home/sasa/shaders/shader3d.vert",
+      "/home/sasa/shaders/shader3d.frag"
+      );
+  program.link();
 
-  gfx::vector3f eye (0, 0, 2);
-  gfx::vector3f lookat (0, 0, 1);
-  gfx::vector3f up (0, 1, 0);
+  int mvp_loc                      = program.getUniformLocation("MVP");
+  gfx::attributes::vpos_location() = program.getAttribLocation("vPos");
+  gfx::attributes::vcol_location() = program.getAttribLocation("vCol");
 
-  vw.data(eye, lookat, up);
 
-  float t = 0;
+  gfx::box box{-1.f, -1.f, 0.4, 0.4};
+  gfx::vbuffer<gfx::vertex3d> vbuff;
+
+  vbuff.bind();
+    vbuff.append(box, gfx::vertex3d(0, 0, 1, gfx::hex(0xd65d0eff)), false);
+  vbuff.load_data();
+
+  gfx::clock timer;
+
+  gfx::view view(width, height);
+  float fov = 1.57;
+
+  gfx::vector3f eye(0, 0, 2.25);
+  gfx::vector3f centre(0, 0,  1);
+  gfx::vector3f up(0, 1,  0);
+
+
+  gfx::vector2f start (-1.f,-1.f);
+  gfx::vector2f mid1  (-1.f, 1.f);
+  gfx::vector2f mid1_2( 0.f, 0.f);
+  gfx::vector2f mid2  ( 1.f, 1.f);
+  gfx::vector2f end   ( 1.f,-1.f);
+
+  gfx::task2d_queue  tasks;
+  tasks.push(gfx::task2d(start, mid1, 1.5, gfx::cubic::ease_inout));
+  tasks.push(gfx::task2d(mid1, mid1_2, 1.5, gfx::cubic::ease_inout));
+  tasks.push(gfx::task2d(mid1_2, mid2, 1.5, gfx::cubic::ease_inout));
+  tasks.push(gfx::task2d(mid2, end, 1.5, gfx::cubic::ease_inout));
+
+  gfx::vector2f start_size(0.4, 0.4);
+  gfx::vector2f end_size(0.3, 0.2);
+  gfx::task2d_queue  box_tasks;
+  box_tasks.push(gfx::task2d(start_size, end_size, 1.5, gfx::cubic::ease_inout));
+  box_tasks.push(gfx::task2d(end_size, end_size, 3.f));
+  box_tasks.push(gfx::task2d(end_size, start_size, 1.5, gfx::cubic::ease_inout));
+
+
+  gfx::task_queue<gfx::task_rgba>   color_tasks;
+  color_tasks.push(gfx::task_rgba(gfx::gruv::red, gfx::gruv::orange, 1.5, gfx::smoothstep));
+  color_tasks.push(gfx::task_rgba(gfx::gruv::orange, gfx::gruv::orange, 3.f, gfx::smoothstep));
+  color_tasks.push(gfx::task_rgba(gfx::gruv::orange, gfx::gruv::red, 1.5, gfx::smoothstep));
 
   glEnable(GL_BLEND);
-  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  glBlendFunc(GL_SRC_COLOR, GL_ONE_MINUS_DST_COLOR);
   glBlendEquation(GL_FUNC_ADD);
-
-  const gfx::vector3f end ( 1.f, -1.f, -1.f);
-  const gfx::vector3f mid1(-1.f , 2.f, -1.f);
-  const gfx::vector3f mid2( 0.3 , 1.f, -1.f);
-
 
   while(!context.should_close())
   {
-    dt = cf.restart();
+    float dt = timer.restart();
 
-    vw.update(window);
-    vw.perspective();
-    vw.multiply();
+    view.update(window);
+    view.data(eye, centre, up);
+    view.perspective(fov, 0.f, 10.f);
+    view.multiply();
 
-    handleInput(window, vertex_buffer, dt, gfx::smoothstep);
+    program.use();
 
-    if (t < 1.f)
+    if(::pressed)
     {
-
-      float st = gfx::cubic::ease_inout(t);
-      auto mid = gfx::lerp(mid1, mid2, st);
-      lerp(vertex_buffer, gfx::lerp(centre, mid, st), gfx::lerp(mid, end, st), size, st);
+      tasks.restart();
+      box_tasks.restart();
+      color_tasks.restart();
     }
 
-    t += dt * 0.3f;
 
-    prg.use();
-    vw.set_mvp(mvp_location);
+    auto pos = tasks.play(dt); 
+    auto size = box_tasks.play(dt);
+    auto color                = color_tasks.play(dt);
+
+    box.x = pos.x;
+    box.y = pos.y;
+
+    box.width  = size.x;
+    box.height = size.y;
+
+    for(int i = 0; i < vbuff.size(); ++i)
+    {
+      vbuff[i].r = color.r;
+      vbuff[i].g = color.g;
+      vbuff[i].b = color.b;
+      vbuff[i].a = color.a;
+    }
+
+    vbuff.modify_box(box, 0, false);
+
+    view.set_mvp(mvp_loc);
+
+    handle_input(window, eye, dt, gfx::smoothstep);
 
     context.clear(gfx::gruv::white, GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-      vertex_buffer.draw(GL_TRIANGLE_STRIP);
+      vbuff.bind();
+      vbuff.draw();
     context.display();
-    
-    if(c.elapsed() > 1.f)
-    {
-      std::cout << "fps--> " << frq << '\n';
-      frq = 0;
-      c.restart();
-    } else {
-      ++frq;
-    }
 
   }
-
-  //contxt.terminate();
   return 0;
 }
 
-void handleInput(GLFWwindow* window, gfx::vbuffer<gfx::vertex3d>& vertex, float dt, std::function<float(float)> ease)
+void handle_input(GLFWwindow* window, gfx::vector3f& eye, float dt, std::function<float(float)> ease)
 {
-  bool updated = false;
-  float speed = 2.5;
+  float speed = 2.5; 
   static float t = 0.f;
-  gfx::vector2f v(0.f, 0.f);
-
-  if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-  {
-    v.x += speed;
-    updated = true;
-  }
+  bool pressed = false;
 
   if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
   {
-    v.x -= speed;
-    updated = true;
+    eye.x -= ease(t) * speed * dt;
+    pressed = true;
+  }
+
+  if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+  {
+    eye.x += ease(t) * speed * dt;
+    pressed = true;
   }
 
   if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
   {
-    v.y += speed;
-    updated = true;
+    eye.z -= ease(t) * speed * dt;
+    pressed = true;
   }
 
   if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
   {
-    v.y -= speed;
-    updated = true;
+    eye.z += ease(t) * speed * dt;
+    pressed = true;
   }
 
-  if(updated) {
-    t += dt * 2;
-  } else 
-  {
-    t -= dt * 5;
-  }
-    vertex.move(gfx::vector2f(ease(t) * dt * v.x, ease(t) * dt * v.y));
-    vertex.modify();
+  if(pressed)
+    t += dt * 0.7;
+  else
+    t -= dt * 3;
+
+  if(t < 0.f)
+    t = 0.f;
 
   if(t > 1.f)
     t = 1.f;
 
-  if(t < 0.f)
-    t = 0.f;
 }
